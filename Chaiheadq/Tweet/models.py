7039067350e97a979470ecdd0459335645d633eb
Muhammad Shaban <muhammadshabansaleem940@gmail.com>
2024-09-06 09:36:46 +0500

    first commit

---
+++ Chaiheadq/Tweet/models.py
@@ -0,0 +1,13 @@
+# models.py
+from django.db import models
+from django.contrib.auth.models import User
+
+class Tweet(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField(max_length=250, default='Default tweet text')
+    photo = models.ImageField(upload_to='photos/', blank=True, null=True)  # Ensure field name is 'photo'
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.user.username} - {self.text[:10]}'
